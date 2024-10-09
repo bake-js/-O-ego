ad41164513eab297536dbb69e42612c920f0301c
deMGoncalves <cleber.engineer@hotmail.com>
2024-10-09 15:12:32 -0300

    feat: implementacao do number field

--- src/textField/index.js
+++ src/textField/index.js
@@ -1,1 +1,2 @@
+import "./numberField";
 import "./textField";
--- src/textField/interfaces.js
+++ src/textField/interfaces.js
@@ -3,5 +3,6 @@ export const invalidated = Symbol("invalidated");
 export const removed = Symbol("removed");
 export const setDisplay = Symbol("setDisplay");
 export const setFormValue = Symbol("setFormValue");
+export const setMask = Symbol("setMask");
 export const setState = Symbol("setState");
 export const setValidity = Symbol("setValidity");
